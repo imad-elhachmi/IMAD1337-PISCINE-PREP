ab31d5f16d216d4a23593f063ba0234204e4d1d3
IMAD EL HACHMI <im.imadelhachmi@gmail.com>
2025-10-28 13:36:37 +0100

    Add files via upload

---
+++ 1337-c-solutions/C00/EX01.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+    // ASCII : a = 97 , z = 122
+    char c = 97 ;
+    while( c <= 122)
+    {
+        write(1 , &c , 1);
+        c++ ;
+    }
+}
+
+/*
+int main(void)
+{
+    ft_print_alphabet();
+    return (0);
+}
+*/
